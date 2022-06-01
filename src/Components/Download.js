6e17a55f3a3d34c23712fe5c22440a7175c14d27
shubham negi <shubham.negi@bitcs.in>
2022-06-01 20:11:16 +0530

    added loader

--- src/Components/Download.js
+++ src/Components/Download.js
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react";
 // import { useLocation } from "react-router-dom"
-import axios from "axios"
-import './../index.css';
-import Chip from '@material-ui/core/Chip'
-
+import axios from "axios";
+import "./../index.css";
+import Chip from "@material-ui/core/Chip";
+import Loader from "./loader";
 
 export function Download() {
-
   // function useQuery() {
   //   return new URLSearchParams(useLocation.search);
   // }
@@ -20,29 +19,38 @@ export function Download() {
   // let query = useQuery();
   const [url, setUrl] = useState("");
   const [title, setTitle] = useState("Loading");
-  const [thumbnail, setThumbnail] = useState("")
+  const [thumbnail, setThumbnail] = useState("");
   const [formats, setFormats] = useState();
   const [audioFormats, setAudioFormats] = useState();
-
+  const [loader, setLoader] = useState(true);
 
   async function fetchData() {
     const str = getURL();
-    setUrl(str)
+    setUrl(str);
     console.log(str);
-    const uTubeVideo = await axios.get(`https://yt-downloader-backend.herokuapp.com/video?videoId=${str}`);
- //https://yt-downloader-backend.herokuapp.com
+    const uTubeVideo = await axios.get(
+      `https://yt-downloader-backend.herokuapp.com/video?videoId=${str}`
+    );
+    //https://yt-downloader-backend.herokuapp.com
 
     if (uTubeVideo) {
       setTitle(uTubeVideo.data.videoDetails.title);
-      setThumbnail(uTubeVideo.data.videoDetails.thumbnails[uTubeVideo.data.videoDetails.thumbnails.length - 1].url)
-     //console.log(uTubeVideo.data.videoDetails.thumbnails[uTubeVideo.data.videoDetails.thumbnails.length - 1].url);
-      setFormats(uTubeVideo.data.formats)
+      setThumbnail(
+        uTubeVideo.data.videoDetails.thumbnails[
+          uTubeVideo.data.videoDetails.thumbnails.length - 1
+        ].url
+      );
+      //console.log(uTubeVideo.data.videoDetails.thumbnails[uTubeVideo.data.videoDetails.thumbnails.length - 1].url);
+      setFormats(uTubeVideo.data.formats);
+      setLoader(false);
     }
   }
 
   async function fetchAudio() {
     const str1 = getURL();
-    const uTubeAudio = await axios.get(`https://yt-downloader-backend.herokuapp.com/audio?videoId=${str1}`);
+    const uTubeAudio = await axios.get(
+      `https://yt-downloader-backend.herokuapp.com/audio?videoId=${str1}`
+    );
     if (uTubeAudio) {
       setAudioFormats(uTubeAudio.data);
     }
@@ -54,31 +62,78 @@ export function Download() {
   }, [window.location.search]);
 
   const downloadFile = (itag, type) => {
-    window.open(`https://yt-downloader-backend.herokuapp.com/download?title=${title}&videoId=${url}&type=${type ? "mp4" : "mp3"}&itag=${itag}`);
-  }
+    window.open(
+      `https://yt-downloader-backend.herokuapp.com/download?title=${title}&videoId=${url}&type=${
+        type ? "mp4" : "mp3"
+      }&itag=${itag}`
+    );
+  };
 
   return (
-    <div className="download_Section">
-      <div className="thumbnail" >
-        <img src={thumbnail} />
-      </div>
-      <h1 className="DownloadSection">{title}</h1>
-      <hr />
+    <>
+      {loader ? (
+        <Loader />
+      ) : (
+        <div className="download_Section">
+          <div className="thumbnail">
+            <img src={thumbnail} />
+          </div>
+          <h1 className="DownloadSection">{title}</h1>
+          <hr />
 
-      <div className="DownloadSection">
-        <h1>Video</h1>
-        {formats && formats.map((format, index) => (
-          format.qualityLabel === null ? "" : format.hasAudio === true ? <Chip className="chip" label={format.qualityLabel} onClick={() => { downloadFile(format.itag, format.hasVideo) }} key={index} style={{ margin: "0rem 0.8rem 0.8rem 1rem", cursor: "pointer", backgroundColor: "red", color: "#fff" }} /> : ""
-        ))}
-      </div>
+          <div className="DownloadSection">
+            <h1>Video</h1>
+            {formats &&
+              formats.map((format, index) =>
+                format.qualityLabel === null ? (
+                  ""
+                ) : format.hasAudio === true ? (
+                  <Chip
+                    className="chip"
+                    label={format.qualityLabel}
+                    onClick={() => {
+                      downloadFile(format.itag, format.hasVideo);
+                    }}
+                    key={index}
+                    style={{
+                      margin: "0rem 0.8rem 0.8rem 1rem",
+                      cursor: "pointer",
+                      backgroundColor: "red",
+                      color: "#fff",
+                    }}
+                  />
+                ) : (
+                  ""
+                )
+              )}
+          </div>
 
-      <div className="DownloadSection">
-        <h1>Audio</h1>
-        {audioFormats && audioFormats.map((format, index) => (
-          <Chip className="chip" label={format.mimeType.split(";")[0] === "audio/mp4" ? "audio/mp3" : format.mimeType.split(";")[0]} onClick={() => { downloadFile(format.itag, format.hasVideo) }} key={index} style={{ margin: "0rem 0.8rem 0.8rem 1rem", cursor: "pointer", backgroundColor: "red", color: "#fff" }} />
-        ))}
-      </div>
-      
-    </div>
-  )
+          <div className="DownloadSection">
+            <h1>Audio</h1>
+            {audioFormats &&
+              audioFormats.map((format, index) => (
+                <Chip
+                  className="chip"
+                  label={
+                    format.mimeType.split(";")[0] === "audio/mp4"
+                      ? "audio/mp3"
+                      : format.mimeType.split(";")[0]
+                  }
+                  onClick={() => {
+                    downloadFile(format.itag, format.hasVideo);
+                  }}
+                  key={index}
+                  style={{
+                    margin: "0rem 0.8rem 0.8rem 1rem",
+                    cursor: "pointer",
+                    backgroundColor: "red",
+                    color: "#fff",
+                  }}
+                />
+              ))}
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
