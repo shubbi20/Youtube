f1b6f28a2c9026742f83bc9bdf773ea559615219
shubham negi <shubham.negi@bitcs.in>
2022-06-01 20:19:53 +0530

    fixed some change

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -1,13 +1,20 @@
-import React from "react"
-import {GiPawHeart} from "react-icons/gi";
+import React from "react";
+import { GiPawHeart } from "react-icons/gi";
 
-export const FooterBox=()=>{
-   return(
-       <div className="footer">
-           <div className="innerfooter">
-           <h2>A project built with <GiPawHeart/> by <a style={{color:'white',fontWeight:'bolder'}} href="https://github.com/shubbi20">@shubhamNegi</a></h2>
-           </div>
-           
-       </div>
-   )
-}
+export const FooterBox = () => {
+  return (
+    <div className="footer">
+      <div className="innerfooter">
+        <h2>
+          A project built with 🖤 by{" "}
+          <a
+            style={{ color: "white", fontWeight: "bolder" }}
+            href="https://github.com/shubbi20"
+          >
+            @shubhamNegi
+          </a>
+        </h2>
+      </div>
+    </div>
+  );
+};
